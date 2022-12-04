8de3d06a5b41228e19a919b1c237262b043cd2e8
Gabriel <gabrielgomesch@gmail.com>
2022-12-04 14:56:49 -0300

    initial commit

---
+++ lib/ruby/regex/training.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "training/version"
+
+module Ruby
+  module Regex
+    module Training
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
